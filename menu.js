4ffd7a324ad673085feed855131968255cb261af
AlbertDev <dorcealbertmary@gmail.com>
2022-11-06 19:22:07 -0500

    btn close added

--- menu.js
+++ menu.js
@@ -1,3 +1,5 @@
 function toggleMobileMenu(){
     document.getElementById("navbar-mobile").classList.toggle("open");
+    document.querySelector("body").classList.toggle("open");
+    document.getElementById("menu").classList.toggle("open");
 }
