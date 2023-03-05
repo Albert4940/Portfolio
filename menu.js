42946aa4460bb252715e6776414a4b74b13b7c4e
Albert4940 <dorcealbertmary@gmail.com>
2023-03-05 02:34:23 +0100

    defined activeLink and get all a tag from a vanbar tag

--- menu.js
+++ menu.js
@@ -3,3 +3,14 @@ function toggleMobileMenu(){
     document.querySelector("body").classList.toggle("open");
     document.getElementById("menu").classList.toggle("open");
 }
+
+
+
+/* 
+id = id of a tag container ex nav  
+*/
+function activeLink(id){
+    const HeaderLinks = document.querySelectorAll("#"+id +" a")
+    console.log(HeaderLinks)
+}
+ activeLink("navbar");
