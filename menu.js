02fcf4fff8a9c8ee2655241adc344a9d1d70060a
Albert4940 <dorcealbertmary@gmail.com>
2023-05-16 11:02:47 -0700

    activeLink function added

--- menu.js
+++ menu.js
@@ -5,12 +5,27 @@ function toggleMobileMenu(){
 }
 
 
-
+function addBackground(e, color){
+    e.target.style.background = color;
+    //console.log(e.target)
+}
 /* 
 id = id of a tag container ex nav  
 */
-function activeLink(id){
-    const HeaderLinks = document.querySelectorAll("#"+id +" a")
-    console.log(HeaderLinks)
+function activeLink(id, color){
+    const LINKS = document.querySelectorAll("#"+id +" a")
+    
+    LINKS.forEach(function(link){
+        link.addEventListener("click", function(e){
+            //remove a background from all links
+            LINKS.forEach(function(link){
+                link.style.background = "transparent";
+            })
+
+            addBackground(e,color)
+        })
+    })
+    
 }
- activeLink("navbar");
+
+ activeLink("navbar", "#45567d");
